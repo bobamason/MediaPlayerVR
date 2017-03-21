e84beb1bc5b8af30e97422a21607c981b35f7630
bobm3 <mason94275@gmail.com>
2017-03-21 16:56:28 -0400

    ADDED FBO

---
+++ app/src/main/java/net/masonapps/mediaplayervr/audiovisualization/tests/FrameBufferAdapter.java
@@ -0,0 +1,168 @@
+package net.masonapps.mediaplayervr.audiovisualization.tests;
+
+import android.annotation.SuppressLint;
+import android.util.Log;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.VertexAttributes;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g3d.Environment;
+import com.badlogic.gdx.graphics.g3d.Material;
+import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.graphics.g3d.ModelBatch;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.environment.PointLight;
+import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+import com.badlogic.gdx.graphics.g3d.utils.ShaderProvider;
+import com.badlogic.gdx.graphics.glutils.FrameBuffer;
+import com.badlogic.gdx.graphics.glutils.ShaderProgram;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.GdxRuntimeException;
+
+/**
+ * Created by Bob on 3/17/2017.
+ */
+
+public class FrameBufferAdapter extends BaseAppAdapter {
+    private final Vector3 axis = new Vector3(1f, 1f, 1f).nor();
+    private Array<ModelInstance> instances = new Array<>();
+    private Environment enviroment;
+    private SpriteBatch spriteBatch;
+    private FrameBuffer fbo;
+    private OrthographicCamera orthoCamera;
+    private float angle = 0f;
+    private Texture texture;
+    private Color color = new Color();
+    private ShaderProgram shaderProgram;
+    private float time = 0f;
+
+
+    @Override
+    protected void onCreate() {
+        setBackgroundColor(Color.DARK_GRAY);
+        enviroment = new Environment();
+        enviroment.set(ColorAttribute.createAmbient(Color.GRAY));
+        PointLight light = new PointLight();
+        light.setPosition(0, 1, 0);
+        light.setColor(Color.WHITE);
+        light.setIntensity(40f);
+        enviroment.add(light);
+        final ModelBuilder modelBuilder = new ModelBuilder();
+
+        texture = new Texture("particle.png");
+
+        spriteBatch = new SpriteBatch();
+        fbo = new FrameBuffer(Pixmap.Format.RGB565, Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), true);
+        orthoCamera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
+        final Color[] colors = new Color[]{Color.LIME, Color.BLUE, Color.OLIVE, Color.ORANGE, Color.TAN, Color.RED, Color.PURPLE};
+        for (int i = 0; i < colors.length; i++) {
+            instances.add(new ModelInstance(createSphere(modelBuilder, colors[i]), 0, 0, -5));
+        }
+
+        createShaderProgram();
+    }
+
+    private void createShaderProgram() {
+        shaderProgram = new ShaderProgram(Gdx.files.internal("shaders/post.vertex.glsl"), Gdx.files.internal("shaders/post.fragment.glsl"));
+        if (!shaderProgram.isCompiled())
+            throw new GdxRuntimeException("could not compile splat batch: " + shaderProgram.getLog());
+        if (shaderProgram.getLog().length() != 0)
+            Gdx.app.log("PuzzleBatch", shaderProgram.getLog());
+    }
+
+    @Override
+    protected ShaderProvider createShaderProvider() {
+        return new PhongShaderProvider();
+    }
+
+    private Model createSphere(ModelBuilder modelBuilder, Color c) {
+        final Material material = new Material(ColorAttribute.createDiffuse(c), ColorAttribute.createAmbient(c), ColorAttribute.createSpecular(Color.WHITE));
+        return modelBuilder.createSphere(1f, 1f, 1f, 24, 16, material, VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal);
+    }
+
+    @Override
+    protected void update() {
+        angle += Gdx.graphics.getDeltaTime() * 30f;
+        for (int i = 0; i < instances.size; i++) {
+            instances.get(i).transform.idt().translate(MathUtils.cosDeg(angle - i * 45f) * 2f, MathUtils.sinDeg(angle + i * 30f) * 3f, -5f - i).rotate(axis, angle + i * 60f);
+        }
+    }
+
+    @SuppressLint("MissingSuperCall")
+    @Override
+    public void render() {
+        if (Gdx.graphics.getFrameId() % 60 == 0)
+            Log.d("FPS", Gdx.graphics.getFramesPerSecond() + "fps");
+
+        fbo.begin();
+
+        Gdx.gl.glViewport(0, 0, fbo.getWidth(), fbo.getHeight());
+        Gdx.gl.glClearColor(backgroundColor.r, backgroundColor.g, backgroundColor.b, backgroundColor.a);
+//        Gdx.gl.glClear(GL20.GL_DEPTH_BUFFER_BIT);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+
+        Gdx.gl.glDisable(GL20.GL_DEPTH_TEST);
+//        spriteBatch.setShader(null);
+        spriteBatch.begin();
+        spriteBatch.setShader(shaderProgram);
+        shaderProgram.setUniformf("u_time", time);
+//        spriteBatch.setColor(color.set(0, 0, 0, 0.05f));
+        spriteBatch.setProjectionMatrix(orthoCamera.combined);
+
+        spriteBatch.setColor(color.set(Color.WHITE));
+
+        spriteBatch.draw(fbo.getColorBufferTexture(), -fbo.getWidth() / 2 + 5, -fbo.getHeight() / 2 + 5, fbo.getWidth() - 10, fbo.getHeight() - 10, 0, 0, fbo.getWidth(), fbo.getHeight(), false, true);
+
+//        spriteBatch.draw(texture, -fbo.getWidth() / 2, -fbo.getHeight() / 2, fbo.getWidth(), fbo.getHeight());
+
+//        spriteBatch.setShader(null);
+//        spriteBatch.setColor(color.set(MathUtils.random(), MathUtils.random(), MathUtils.random(), 1f));
+//        spriteBatch.draw(texture, Gdx.input.getX() - fbo.getWidth() / 2f - texture.getWidth() / 2f, Gdx.input.getY() - fbo.getHeight() / 2 - texture.getHeight() / 2f, texture.getWidth(), texture.getHeight(), 0, 0, texture.getWidth(), texture.getHeight(), false, true);
+
+        spriteBatch.end();
+        Gdx.gl.glEnable(GL20.GL_DEPTH_TEST);
+
+        update();
+        getCamera().update();
+        getModelBatch().begin(getCamera());
+        render(getModelBatch());
+        getModelBatch().end();
+
+        fbo.end();
+
+        Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        Gdx.gl.glClearColor(backgroundColor.r, backgroundColor.g, backgroundColor.b, backgroundColor.a);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+        time += Gdx.graphics.getDeltaTime();
+        time %= MathUtils.PI2;
+        spriteBatch.setShader(null);
+        spriteBatch.begin();
+//        shaderProgram.setUniformf("u_time", time);
+        spriteBatch.setColor(Color.WHITE);
+        spriteBatch.setProjectionMatrix(orthoCamera.combined);
+        spriteBatch.draw(fbo.getColorBufferTexture(), -fbo.getWidth() / 2, -fbo.getHeight() / 2, fbo.getWidth(), fbo.getHeight());
+        spriteBatch.end();
+    }
+
+    @Override
+    protected void render(ModelBatch modelBatch) {
+        modelBatch.render(instances, enviroment);
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        fbo.dispose();
+        spriteBatch.dispose();
+        texture.dispose();
+    }
+}
