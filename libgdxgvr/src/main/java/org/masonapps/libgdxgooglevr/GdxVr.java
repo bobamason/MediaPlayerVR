ddf32daef3475c36131327db4cb1c1024bd8d707
bobm3 <mason94275@gmail.com>
2017-06-11 17:06:01 -0400

    changes to library

--- libgdxgvr/src/main/java/org/masonapps/libgdxgooglevr/GdxVr.java
+++ libgdxgvr/src/main/java/org/masonapps/libgdxgooglevr/GdxVr.java
@@ -2,10 +2,8 @@ package org.masonapps.libgdxgooglevr;
 
 import com.badlogic.gdx.Audio;
 import com.badlogic.gdx.Files;
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Net;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.GL30;
 
 import org.masonapps.libgdxgooglevr.vr.VrActivity;
 import org.masonapps.libgdxgooglevr.vr.VrAndroidInput;
@@ -16,14 +14,13 @@ import org.masonapps.libgdxgooglevr.vr.VrGraphics;
  */
 
 public class GdxVr {
-    public static VrActivity app = (VrActivity) Gdx.app;
-    public static VrGraphics graphics = (VrGraphics) Gdx.graphics;
-    public static VrAndroidInput input = (VrAndroidInput) Gdx.input;
-    public static Audio audio = Gdx.audio;
-    public static Files files = Gdx.files;
-    public static Net net = Gdx.net;
+    public static VrActivity app;
+    public static VrGraphics graphics;
+    public static VrAndroidInput input;
+    public static Audio audio;
+    public static Files files;
+    public static Net net;
 
-    public static GL20 gl = Gdx.gl;
-    public static GL20 gl20 = Gdx.gl20;
-    public static GL30 gl30 = Gdx.gl30;
+    public static GL20 gl;
+    public static GL20 gl20;
 }
